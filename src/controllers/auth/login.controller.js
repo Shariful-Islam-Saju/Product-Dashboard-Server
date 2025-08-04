9a0bfbb2ac6283b9e6607ecd24fe09081e907faa
Saju <sajukhan12905@gmail.com>
2025-08-04 13:31:48 +0600

    set the token

--- src/controllers/auth/login.controller.js
+++ src/controllers/auth/login.controller.js
@@ -48,9 +48,8 @@ export const loginUser = async (req, res) => {
     res.cookie("Auth_Token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      path: "/",
     });
 
     res.status(200).json({
